bcef99ed82d67ed45fc0bb70efeb504ca47cbb54
Priyansh Dugar <priyansh10jan@gmail.com>
2025-12-25 11:01:48 +0530

    updating UI

--- script.js
+++ script.js
@@ -5,18 +5,26 @@ const years = document.querySelector("#years");
 const price = document.querySelector("#price");
 
 calBtn.addEventListener("click" , function(){
-    calculateHours();
+    let w = parseFloat(wage.value);
+    let y = parseFloat(years.value);
+    let p = parseFloat(price.value);
+    calculateHours(w,p);
     calculateWealth();
 })
 
 resetBtn.addEventListener("click" , function(){
-
+    wage.value = "";
+    years.value = "";
+    price.value = "";
+    console.clear();
 })
 
-function calculateHours(){
-    
+function calculateHours(p,w){
+    let hours = p/w;
+    console.log(hours);
+    return hours;
 }
 
 function calculateWealth(){
-
+    
 }
