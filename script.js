35f059e317b3ae6533d77a3a0795fc5492f300a1
Priyansh Dugar <priyansh10jan@gmail.com>
2025-12-25 10:39:21 +0530

    adding js

---
+++ script.js
@@ -0,0 +1,22 @@
+const calBtn = document.querySelector("#calculate-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const wage = document.querySelector("#wage");
+const years = document.querySelector("#years");
+const price = document.querySelector("#price");
+
+calBtn.addEventListener("click" , function(){
+    calculateHours();
+    calculateWealth();
+})
+
+resetBtn.addEventListener("click" , function(){
+
+})
+
+function calculateHours(){
+    
+}
+
+function calculateWealth(){
+
+}
